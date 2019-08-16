7758f8f5c3a3fd75322905077697d60520705f68
Mahantesh S K <mahantesh.kumbar@inszoom.com>
2019-08-16 11:33:57 -0700

    Elastic Search NEST upgrade

---
+++ NestFullFrameworkExample/ElasticSearchLogic.cs
@@ -0,0 +1,31 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestFullFrameworkExample
+{
+    public class ElasticSearchLogic
+    {
+        public async Task Search1()
+        {
+            try
+            {
+                var _settings = new ConnectionSettings(new Uri("http://testlogstashelasticendpoint.com:80/"));
+
+                var client = new ElasticClient(_settings);
+
+                var bnfResult = await client.SearchAsync<dynamic>(s => s
+                                        .Index("test_index")
+                                        .Query(q => q.Raw("{\"bool\":{\"must\":[{\"term\":{\"testid\":\"oformstg\"}}]}}")));
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ StarterProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using NestFullFrameworkExample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarterProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ElasticSearchLogic elasticSearchLogic = new ElasticSearchLogic();
+            elasticSearchLogic.Search1();
+        }
+    }
+}
